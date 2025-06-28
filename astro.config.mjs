d47f2e0d197dbcdcce40f5cc8a3b23a787efc762
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2025-06-28 08:38:02 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://examprep-pro.com',
+	site: 'https://eduware.vercel.app',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		shikiConfig: {
